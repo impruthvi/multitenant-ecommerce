a1d08091eafdaca00ac572e7ae29bcba1a934daf
impruthvi <pruthvirajput97@gmail.com>
2025-04-20 13:42:11 +0530

    feat: implement dropdown functionality for category selection and add subcategory menu

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -6,16 +6,18 @@ interface CategoriesProps {
 }
 export const Categories = ({ data }: CategoriesProps) => {
   return (
-    <div className="">
-      {data.map((category: Category) => (
-        <div className="" key={category.id}>
-          <CategoryDropdown
-            category={category}
-            isActive={false} // TODO: Implement active state
-            isNavigationHover={false} // TODO: Implement hover state
-          />
-        </div>
-      ))}
+    <div className="relative w-full">
+      <div className="flex flex-nowrap items-center">
+        {data.map((category: Category) => (
+          <div className="" key={category.id}>
+            <CategoryDropdown
+              category={category}
+              isActive={false} // TODO: Implement active state
+              isNavigationHover={false} // TODO: Implement hover state
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
