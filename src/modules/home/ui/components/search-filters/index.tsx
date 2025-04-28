62cf531a55dcbb19e2bd28a481e5076c6fe343a6
impruthvi <pruthvirajput97@gmail.com>
2025-04-28 11:39:05 +0530

    fix(search-filters): correct class name typo in SearchFilters component

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -32,7 +32,7 @@ export const SearchFilters = () => {
 
   return (
     <div
-      className="px-4 lf:px-12 py-8 border-b flex flex-col gap-4 w-full"
+      className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
       style={{
         backgroundColor: activeCategoryColor,
       }}
