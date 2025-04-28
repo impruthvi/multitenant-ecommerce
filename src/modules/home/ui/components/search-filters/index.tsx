62a907a14c5f4b3ebbd593efd4813db2f00c10f5
impruthvi <pruthvirajput97@gmail.com>
2025-04-28 11:35:21 +0530

    feat(breadcrumb): implement Breadcrumb components for navigation and add BreadcrumbNavigation to SearchFilters

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,24 +1,51 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { useTRPC } from "@/trpc/client";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
+import { BreadcrumbNavigation } from "./breadcrumb-navigation";
 
 export const SearchFilters = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+
+  const params = useParams();
+  const categoryParams = params.category as string | undefined;
+  const activeCategory = categoryParams || "all";
+
+  const activeCategoryData = data.find(
+    (category) => category.slug === activeCategory
+  );
+
+  const activeCategoryColor = activeCategoryData?.color || DEFAULT_BG_COLOR;
+  const activeCategoryName = activeCategoryData?.name || null;
+
+  const activeSubCategory = params.subcategory as string | undefined;
+  const activeSubCategoryName =
+    activeCategoryData?.subcategories?.find(
+      (subcategory) => subcategory.slug === activeSubCategory
+    )?.name || null;
+
   return (
     <div
       className="px-4 lf:px-12 py-8 border-b flex flex-col gap-4 w-full"
       style={{
-        backgroundColor: "#F5F5F5",
+        backgroundColor: activeCategoryColor,
       }}
     >
       <SearchInput />
       <div className="hidden lg:block">
         <Categories data={data} />
       </div>
+      <BreadcrumbNavigation
+        activeCategory={activeCategory}
+        activeCategoryName={activeCategoryName}
+        activeSubCategoryName={activeSubCategoryName}
+      />
     </div>
   );
 };
