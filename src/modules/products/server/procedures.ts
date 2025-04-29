471a75cba0e31f2123c9663f3c97e3b9c67930bd
impruthvi <pruthvirajput97@gmail.com>
2025-04-29 14:07:00 +0530

    feat(products): add price filtering functionality and integrate ProductFilters component

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -6,10 +6,28 @@ import { Category } from "@/payload-types";
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
         .input(z.object({
-            category: z.string().optional(),
+            category: z.string().nullable().optional(),
+            minPrice: z.number().nullable().optional(),
+            maxPrice: z.number().nullable().optional(),
         }))
         .query(async ({ ctx, input }) => {
             const where: Where = {};
+
+            if (input.minPrice && input.maxPrice) {
+                where.price = {
+                    less_than_equal: input.maxPrice,
+                    greater_than_equal: input.minPrice,
+                };
+            } else if (input.minPrice) {
+                where.price = {
+                    greater_than_equal: input.minPrice,
+                };
+            } else if (input.maxPrice) {
+                where.price = {
+                    less_than_equal: input.maxPrice,
+                };
+            }
+
             if (input.category) {
                 const categoriesData = await ctx.db.find({
                     collection: "categories",
