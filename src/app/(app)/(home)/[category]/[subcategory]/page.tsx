72eedf6e0aec2a49f608a6b80a6337e3186628da
impruthvi <pruthvirajput97@gmail.com>
2025-04-30 12:22:09 +0530

    feat(products): update category and subcategory pages to use infinite query for product fetching

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -5,6 +5,7 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 interface Props {
   params: Promise<{
     subcategory: string;
@@ -16,10 +17,11 @@ const SubCategoryPage = async ({ params, searchParams }: Props) => {
   const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({
-      category: subcategory,
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
       ...filters,
+      category: subcategory,
+      limit: DEFAULT_LIMIT,
     })
   );
 
--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -8,6 +8,7 @@ import { getQueryClient, trpc } from "@/trpc/server";
 
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 interface Props {
   params: Promise<{
     category: string;
@@ -19,10 +20,11 @@ const CategoryPage = async ({ params, searchParams }: Props) => {
   const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({
-      category,
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
       ...filters,
+      category,
+      limit: DEFAULT_LIMIT,
     })
   );
 
