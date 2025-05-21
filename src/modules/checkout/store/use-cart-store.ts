2c639ecf854d6b887d5e301f7f8fb905d924a1c3
impruthvi <pruthvirajput97@gmail.com>
2025-05-21 11:50:16 +0530

    feat(cart): implement cart functionality with Zustand for state management

---
+++ src/modules/checkout/store/use-cart-store.ts
@@ -0,0 +1,60 @@
+import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+
+interface TenantCart {
+    productIds: string[]
+}
+
+interface CartState {
+    tenantCart: Record<string, TenantCart>
+    addProduct: (tenantSlug: string, productId: string) => void
+    removeProduct: (tenantSlug: string, productId: string) => void
+    clearCart: (tenantSlug: string) => void
+    clearAllCarts: () => void
+    getCartByTenant: (tenantSlug: string) => string[]
+}
+
+export const useCartStore = create<CartState>()(
+    persist(
+        (set, get) => ({
+            tenantCart: {},
+            addProduct: (tenantSlug, productId) =>
+                set((state) => {
+                    const cart = state.tenantCart[tenantSlug] || { productIds: [] }
+                    if (!cart.productIds.includes(productId)) {
+                        cart.productIds.push(productId)
+                    }
+                    return {
+                        tenantCart: {
+                            ...state.tenantCart,
+                            [tenantSlug]: cart,
+                        },
+                    }
+                }),
+            removeProduct: (tenantSlug, productId) =>
+                set((state) => {
+                    const cart = state.tenantCart[tenantSlug]
+                    if (!cart) return state
+                    return {
+                        tenantCart: {
+                            ...state.tenantCart,
+                            [tenantSlug]: {
+                                ...cart,
+                                productIds: cart.productIds.filter((id) => id !== productId)
+                            }
+                        }
+                    }
+                }),
+            clearCart: (tenantSlug) =>
+                set((state) => ({
+                    tenantCart: { ...state.tenantCart, [tenantSlug]: { productIds: [] } },
+                })),
+            clearAllCarts: () => set({ tenantCart: {} }),
+            getCartByTenant: (tenantSlug) => get().tenantCart[tenantSlug]?.productIds || [],
+        }),
+        {
+            name: "storehub-cart",
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+)
