974282ef52aa46264bbe1fdd4b23b147ff60a443
impruthvi <pruthvirajput97@gmail.com>
2025-06-09 22:27:11 +0530

    feat(checkout): integrate Stripe for payment processing and add checkout procedures

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useCart } from "../../hooks/user-cart";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -9,13 +9,17 @@ import { generateTenantUrl } from "@/lib/utils";
 import { CheckoutItem } from "../components/checkout-items";
 import { CheckoutSidebar } from "../components/checkout-sidebar";
 import { InboxIcon, LoaderIcon } from "lucide-react";
+import { useCheckoutStates } from "../../hooks/use-checkout-states";
+import { useRouter } from "next/navigation";
 
 interface CheckoutViewProps {
   tenantSlug: string;
 }
 
 export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
-  const { productIds, clearAllCarts, removeProduct } = useCart(tenantSlug);
+  const router = useRouter();
+  const [states,setStates] = useCheckoutStates();
+  const { productIds, clearCart, removeProduct } = useCart(tenantSlug);
   const trpc = useTRPC();
   const { data, error, isLoading } = useQuery(
     trpc.checkout.getProducts.queryOptions({
@@ -23,12 +27,38 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
     })
   );
 
+  const purchase = useMutation(trpc.checkout.purchase.mutationOptions({
+    onMutate: () => {
+      setStates({ success: false, cancel: false });
+    },
+    onSuccess: (data) => {
+      window.location.href = data.url
+    },
+    onError: (error) => {
+      if(error.data?.code === "UNAUTHORIZED") {
+        // TODO: Modify when subdomain enabled
+        router.push("/sign-in")
+      }
+
+      toast.error(error.message || "Failed to initiate checkout. Please try again.");
+    }
+  }));
+
   useEffect(() => {
     if (error?.data?.code === "NOT_FOUND") {
-      clearAllCarts();
+      clearCart();
       toast.warning("Invalid cart, clearing all carts.");
     }
-  }, [error, clearAllCarts]);
+  }, [error, clearCart]);
+
+  useEffect(() => {
+    if (states.success) {
+      setStates({ success: false, cancel: false });
+      clearCart();
+      // TODO: Add Invalidate library
+      router.push("/products");
+    }
+  }, [states.success, clearCart, router, setStates]);
 
   if (isLoading)
     return (
@@ -74,9 +104,9 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
         <div className="lg:col-span-2">
           <CheckoutSidebar
             total={data?.totalPrice || 0}
-            onCheckout={() => {}}
-            isCanceled={false}
-            isPending={false}
+            onPurchase={() => purchase.mutate({ productIds, tenantSlug })}
+            isCanceled={states.cancel}
+            disabled={purchase.isPending}
           />
         </div>
       </div>
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -2,7 +2,8 @@ import { cache } from 'react';
 import { getPayload } from 'payload';
 import config from '@payload-config';
 import superjson from 'superjson';
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
+import { headers as getHeaders } from 'next/headers';
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -26,4 +27,28 @@ export const baseProcedure = t.procedure.use(async ({ next }) => {
   const payload = await getPayload({ config });
 
   return next({ ctx: { db: payload } })
+});
+
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const headers = await getHeaders();
+
+  const session = await ctx.db.auth({ headers });
+
+  if (!session.user) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'You must be logged in to perform this action.',
+    });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      session: {
+        ...session,
+        user: session.user,
+      }
+    },
+  });
+
 });
