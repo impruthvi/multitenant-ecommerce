7cba3a5a76d73487da30d2a0af7850ce3335f348
impruthvi <pruthvirajput97@gmail.com>
2025-06-21 15:59:18 +0530

    fix(product-list): update no products found message to specify purchased products in library

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -44,7 +44,7 @@ export const ProductList = ({
       <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
         <InboxIcon />
         <p className="text-base font-medium">
-          No products found for this category.
+          No purchased products found in your library.
         </p>
       </div>
     );
